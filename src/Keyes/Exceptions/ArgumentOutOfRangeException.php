8e7ecc2b1fbc3118c48314d778ea2ae57a9f8179
Karel Saroch <choraslerak@gmail.com>
2018-03-16 13:34:50 +0100

    ArgumentOutOfRangeException.php

---
+++ src/Keyes/Exceptions/ArgumentOutOfRangeException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Keyes;
+
+/**
+ * The exception that is thrown when the value of an argument is
+ * outside the allowable range of values as defined by the invoked method.
+ */
+class ArgumentOutOfRangeException extends \InvalidArgumentException
+{
+}
+
+?>
