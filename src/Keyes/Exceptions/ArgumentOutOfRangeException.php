ff1dec5328c47b294e4eb2e15dd0bd6190894515
Karel Saroch <choraslerak@gmail.com>
2018-03-16 14:04:13 +0100

    InvalidArgumentException

--- src/Keyes/Exceptions/ArgumentOutOfRangeException.php
+++ src/Keyes/Exceptions/ArgumentOutOfRangeException.php
@@ -5,7 +5,7 @@ namespace Keyes;
  * The exception that is thrown when the value of an argument is
  * outside the allowable range of values as defined by the invoked method.
  */
-class ArgumentOutOfRangeException extends \InvalidArgumentException
+class ArgumentOutOfRangeException extends InvalidArgumentException
 {
 }
 
---
+++ src/Keyes/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,11 @@
+<?php
+namespace Keyes;
+
+/**
+ * The exception that is thrown when an argument does not match with the expected value.
+ */
+class InvalidArgumentException extends \InvalidArgumentException
+{
+}
+
+?>
